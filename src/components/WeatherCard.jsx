b4e8969a3f2a6e12834e189def2dab8e1cf486d9
sherif rabiw karim <rabiwkarim55@gmail.com>
2025-08-22 19:03:50 +0000

    new tailwind conigf

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,6 +1,6 @@
 function WeatherCard({city, country}){
 
-	return( <div>
+	return( <div className="bg-blue-500 text-white font-bold rounded m-20 p-50 w-150" >
 		<h1>{country}</h1>
 		<p>{city}</p>
 	</div>);
